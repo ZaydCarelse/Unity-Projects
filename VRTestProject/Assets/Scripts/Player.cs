5d87e40e2a1a7f6080b47929f870363e14f93d24
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-18 21:03:59 +0200

    VR Test Project Initial Commit
    Added Google VR SDK and built test scene

---
+++ VRTestProject/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
