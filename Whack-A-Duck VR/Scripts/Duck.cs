0fc47dce8f7472ad8b49c7cc6dfb6e617f226303
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-21 15:07:48 +0200

    Whack-A-Duck Initial Commit
    - Added PlayerController class
    - Added Duck Class

---
+++ Whack-A-Duck VR/Scripts/Duck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck : MonoBehaviour
+{
+    [Header("Duck Physical Constraints:")]
+    public float visibleHeight = 0.2f;
+    public float hiddenHeight = -0.3f;
+
+    [Header("Gameplay Mechanics:")]
+    public float speed = 4f;
+
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = new Vector3(transform.localPosition.x, hiddenHeight, transform.localPosition.z);
+        transform.localPosition = targetPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
+    }
+
+    public void OnHit()
+    {
+        targetPosition = new Vector3(transform.localPosition.x, hiddenHeight, transform.localPosition.z);
+    }
+}
---
+++ Whack-A-Duck VR/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                if (hit.transform.GetComponent<Duck>() != null)
+                {
+                    Duck duck = hit.transform.GetComponent<Duck>();
+                    duck.OnHit();
+                }
+            }
+        }
+    }
+}
