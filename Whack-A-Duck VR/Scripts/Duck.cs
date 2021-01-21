7405eae73fe31fdf1c71fa35602e862961271ff6
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-21 16:30:30 +0200

    Second Commit

--- Whack-A-Duck VR/Scripts/Duck.cs
+++ Whack-A-Duck VR/Scripts/Duck.cs
@@ -10,11 +10,13 @@ public class Duck : MonoBehaviour
 
     [Header("Gameplay Mechanics:")]
     public float speed = 4f;
+    public float disappearDuration = 1.25f;
 
     private Vector3 targetPosition;
+    private float disappearTimer = 0f;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         targetPosition = new Vector3(transform.localPosition.x, hiddenHeight, transform.localPosition.z);
         transform.localPosition = targetPosition;
@@ -23,11 +25,30 @@ public class Duck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        disappearTimer -= Time.deltaTime;
+        if (disappearTimer <= 0f)
+        {
+            Hide();
+        }
+
         transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
+    }
+
+    public void Hide()
+    {
+        targetPosition = new Vector3(transform.localPosition.x, hiddenHeight, transform.localPosition.z);
+    }
+
+    public void Rise()
+    {
+        targetPosition = new Vector3(transform.localPosition.x, visibleHeight, transform.localPosition.z);
+
+        disappearTimer = disappearDuration;
+
     }
 
     public void OnHit()
     {
-        targetPosition = new Vector3(transform.localPosition.x, hiddenHeight, transform.localPosition.z);
+        Hide();
     }
 }
---
+++ Whack-A-Duck VR/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [Header("Game Management:")]
+    public GameObject duckContainer;
+    public float spawnDuration = 1.5f;
+    public float spawnDecrease = 0.1f;
+    public float minSpawnDuration = 0.5f;
+    public float gameTimer = 30f;
+    public TextMeshProUGUI infoText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI hiscoreText;
+
+    private Duck[] ducks;
+    private float spawnTimer = 0f;
+
+    private void Awake()
+    {
+        instance = this;    
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ducks = duckContainer.GetComponentsInChildren<Duck>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            ducks[Random.Range(0, ducks.Length)].Rise();
+
+            spawnDuration -= spawnDecrease;
+            if(spawnDuration < minSpawnDuration)
+            {
+                spawnDuration = minSpawnDuration;
+            }
+            spawnTimer = spawnDuration;
+        }
+
+        gameTimer -= Time.deltaTime;
+        if (gameTimer > 0f)
+        {
+            infoText.text = "HIT ALL THE DUCKS!\nTIME: " + Mathf.Floor(gameTimer);
+        }
+    }
+}
