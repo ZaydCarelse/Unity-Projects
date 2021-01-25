910d8bf25b5be9d45bfce2e686aed10de4c1b0a7
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-25 19:13:25 +0200

    Initial Project setup and Camera Controller creation
    - Set up the project and implemented a startegy game like camera for the viewport.
    - The camera is parented to a camera rig which offsets the camera position.

---
+++ Traffic Simulation Project (Fuzzy Logic)/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Camera Motion:")]
+    public float movementSpeed;
+    public float movementTime;
+
+    [Header("Camera Positioning")]
+    public Vector3 newPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HandleMovementInput()
+    {
+
+        // Zooms the camera in
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * movementSpeed);
+        }
+
+        // Zooms the camera out
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed);
+        }
+    }
+}
