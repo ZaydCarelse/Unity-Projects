9c198597c74d5803e022add4bf5915e806cbe1ac
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-01-26 10:15:51 +0200

    Finished Camera Controller

---
+++ Traffic Simulation Project/Scripts/CarController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        CameraController.instance.followTransform = transform;
+    }
+}
---
+++ Traffic Simulation Project/Scripts/PedestrianController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestrianController : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        CameraController.instance.followTransform = transform;
+    }
+}
