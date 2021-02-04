84beb84247d3af8f86159a71548372803f3f2cb4
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-02-04 12:23:39 +0200

    Added jump and jump bounce

---
+++ Track and Field VR/Scripts/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [Header("Running Motion Variables:")]
+    public float bounceAmp = 0.1f;
+    public float bounceFreq = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x,
+            Mathf.Cos(transform.position.z * bounceFreq) * bounceAmp,
+            transform.localPosition.z);
+    }
+}
--- Track and Field VR/Scripts/PlayerController.cs
+++ Track and Field VR/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement variables:")]
+    public float speed = 5.5f;
+    public float jumpForce = 450f;
+    public float coolDown = 1.5f;
+    private float jumpTimer = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,18 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //moves the player forward
+        transform.position += speed * Vector3.forward * Time.deltaTime;
+
+        //makes the player jump
+        jumpTimer -= Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump"))
+        {
+            if (jumpTimer <= 0f)
+            {
+                jumpTimer = coolDown;
+                GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+            }
+        }
     }
 }
