0f93e719f22c4418a41749c194935c67d0e98acf
Zayd Carelse <36879135+ZaydCarelse@users.noreply.github.com>
2021-03-07 00:45:15 +0200

    Initial Commit

---
+++ Tower Defence Game/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if(wavePointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavePointIndex++;
+        target = Waypoints.points[wavePointIndex];
+    }
+}
+
